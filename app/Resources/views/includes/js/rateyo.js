6be651e1aa62e7374e0650599f343c0fab2eea02
jdiaz-gecko <jdiaz@gecko-btl.com>
2016-11-15 13:59:01 -0500

    cambios v.60 :tennis:

--- app/Resources/views/includes/js/rateyo.js
+++ app/Resources/views/includes/js/rateyo.js
@@ -3,7 +3,7 @@ var color="#30BDEF";
 $( ".rateYo_negocios" ).each(function( index ) {
     $(this).rateYo({
         rating: $(this).attr("data-rating"),
-        starWidth: "20px",
+        starWidth: "18px",
         readOnly: true,
         ratedFill: color,
         normalFill: "#A0A0A0"
