9644effd435c9347d3ec27a045b69dbb25fb0c8a
synry63 <synry63@gmail.com>
2017-02-01 20:35:05 -0500

    add coment popup to public profile

--- src/AppBundle/Controller/ComentarioNegocioController.php
+++ src/AppBundle/Controller/ComentarioNegocioController.php
@@ -68,6 +68,109 @@ class ComentarioNegocioController extends Controller
             );
         }
     }
+    public function comentarNegocioPerfilPublicoNewAction($slug_negocio,$id,Request $request){
+
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $negocio = $this->getDoctrine()->getRepository('AppBundle:Negocio')->findOneBy(array('slug'=>$slug_negocio));
+        if(is_object($user)){
+            //$comentarioProveedor = $this->getDoctrine()->getRepository('AppBundle:ComentarioProveedor')
+            // ->findOneBy(array('proveedor'=>$proveedor,'user'=>$user));
+            $comentarioNegocio = new ComentarioNegocio();
+
+            $form = $this->createForm(new ComentarioNegocioType(),$comentarioNegocio,array(
+                'action' => $this->generateUrl('negocio_perfil_publico_comentar_new',array('slug_negocio'=>$slug_negocio,'id'=>$id)),
+                'method' => 'POST',
+            ));
+            //$form = $this->createForm(new ComentarioProveedorType(), $comentarioProveedor);
+            $form->handleRequest($request);
+            if ($form->isSubmitted()) {
+                if($form->isValid()){
+                    $comentarioNegocio->setUser($user);
+                    $comentarioNegocio->setNegocio($negocio);
+
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($comentarioNegocio);
+                    $em->flush();
+
+                    $request->getSession()->getFlashBag()->add('success', 'Gracias por tu comentario !');
+                    //return $this->redirectToRoute('proveedor_detail',array('slug_site'=>$slug_site,'slug_proveedor'=>$slug_proveedor));
+                    $url = $this->generateUrl('profile_public',array('id'=>$id));
+                    $response = new JsonResponse();
+                    $response->setData(array(
+                        'success' => $url
+                    ));
+                    return $response;
+                    //return $this->redirectToRoute('task_success');
+                    //$this->redirect($request->getReferer());
+                }
+                else{
+                    $errors = $this->get('form_serializer')->serializeFormErrors($form, true, true);
+                    $response = new JsonResponse();
+                    $response->setData(array(
+                        'errors' => $errors
+                    ));
+                    return $response;
+                }
+            }
+            //$renderOut['form'] = $form->createView();
+            return $this->render(
+                'comentario_negocio.html.twig',array(
+                    'form'=>$form->createView()
+                )
+            );
+        }
+    }
+    public function comentarNegocioPerfilPublicoEditAction($slug_negocio,$id,Request $request){
+
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $negocio = $this->getDoctrine()->getRepository('AppBundle:Negocio')->findOneBy(array('slug'=>$slug_negocio));
+        if(is_object($user)){
+            $comentarioNegocio = $this->getDoctrine()->getRepository('AppBundle:ComentarioNegocio')
+                ->findOneBy(array('negocio'=>$negocio,'user'=>$user));
+
+            $form = $this->createForm(new ComentarioNegocioType(),$comentarioNegocio,array(
+                'action' => $this->generateUrl('negocio_perfil_publico_comentar_edit',array('slug_negocio'=>$slug_negocio,'id'=>$id)),
+                'method' => 'POST',
+            ));
+            //$form = $this->createForm(new ComentarioProveedorType(), $comentarioProveedor);
+            $form->handleRequest($request);
+            if ($form->isSubmitted()) {
+                if($form->isValid()){
+                    $comentarioNegocio->setUser($user);
+                    $comentarioNegocio->setNegocio($negocio);
+
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($comentarioNegocio);
+                    $em->flush();
+
+                    $request->getSession()->getFlashBag()->add('success', 'Gracias por tu comentario !');
+                    //return $this->redirectToRoute('proveedor_detail',array('slug_site'=>$slug_site,'slug_proveedor'=>$slug_proveedor));
+                    $url = $this->generateUrl('profile_public',array('id'=>$id));
+                    $response = new JsonResponse();
+                    $response->setData(array(
+                        'success' => $url
+                    ));
+                    return $response;
+                    //return $this->redirectToRoute('task_success');
+                    //$this->redirect($request->getReferer());
+                }
+                else{
+                    $errors = $this->get('form_serializer')->serializeFormErrors($form, true, true);
+                    $response = new JsonResponse();
+                    $response->setData(array(
+                        'errors' => $errors
+                    ));
+                    return $response;
+                }
+            }
+            //$renderOut['form'] = $form->createView();
+            return $this->render(
+                'comentario_negocio.html.twig',array(
+                    'form'=>$form->createView()
+                )
+            );
+        }
+    }
     public function comentarNegocioUserEditAction($slug_negocio,Request $request){
         $user = $this->container->get('security.context')->getToken()->getUser();
         $negocio = $this->getDoctrine()->getRepository('AppBundle:Negocio')->findOneBy(array('slug'=>$slug_negocio));
